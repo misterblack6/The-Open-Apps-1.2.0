cef1473a6aa60e4ee47479a4228b9ff62724eba4
MrblackDeveloper_87 <102394232+misterblack6@users.noreply.github.com>
2022-11-30 10:56:52 -0500

    Add files via upload

---
+++ The Open Apps.bat
@@ -0,0 +1,390 @@
+﻿::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCiDJHyG8VAkJ1t5SQ6LPSypRoIZ6tf95uaCsXEUV+0xLsHS2bvu
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZkoaHGQ=
+::ZQ05rAF9IBncCkqN+0xwdVsFAlXMbAs=
+::ZQ05rAF9IAHYFVzEqQIEIB57XAGMHXuqFdU=
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQIdOhlYTQeJZiuODrAz+O31zvqXsS0=
+::dhA7uBVwLU+EWHSW9k4xKxAUdQuXKH64A9U=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE100gMQldSwyWfCOZT/Ux+ur37umM4nQSQfYqeoq7
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRml9lA1aBhBTQiTKWK/FPUd+OTy7OuEq+7OWqI+dM/S1KeaIe4H7wSE
+::Zh4grVQjdCmDJGyX8VAjFD5WSRbCGGqsD7EgzO3o5P6IsnEpXOd/V5/e1PSvMPAA1nHscNYCxn9U2OocHAgWewquDg==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+﻿@ECHO OFF
+REM BFCPEOPTIONSTART
+REM www.youtube.com/@Mrblack_87
+REM BFCPEEXE=
+REM BFCPEICON=
+REM BFCPEICONINDEX=-1
+REM BFCPEEMBEDDISPLAY=0
+REM BFCPEEMBEDDELETE=1
+REM BFCPEADMINEXE=0
+REM BFCPEINVISEXE=0
+REM BFCPEVERINCLUDE=0
+REM BFCPEVERVERSION=1.1.0.0
+REM BFCPEVERPRODUCT=The Open Apps (R)
+REM BFCPEVERDESC= Aplicación para principiantes
+REM BFCPEVERCOMPANY= Mrblack Youtube
+REM BFCPEVERCOPYRIGHT= Copyright (C) Mrblack's Youtube; All rights Reserved
+REM BFCPEOPTIONEND
+@ECHO ON
+@echo off
+title Mrblack: The Open Apps (V1.1)
+color e
+mode 105,55
+:inicio
+echo.
+echo.
+echo        --------------------------------------------------------------------------------
+echo=                               Menu,que quires abrir?
+echo              -----------------------------------------------------------------
+echo=
+echo=                          1. abrir calculadora
+echo=                          2. abrir youtube
+echo=                          3. abrir panel de control
+echo=                          4. abrir administrador de tareas
+echo=                          5. abrir explorador de archivos
+echo=                          6. abrir bloc de notas
+echo=                          7. ver informacion de windows
+echo=                          8. paint dibuja con el paint
+echo=                          9. abrir google chrome investiga!
+echo=                          10.abre word y escribe lo que quiras!
+echo=                          11.abre el dialogo de graficos de windows
+echo=                          12.abre la carpeta %appdata%
+echo=                          13.abre Excel
+echo=                          14.abre la configuracion del sistema
+echo=                          15.apaga tu computadora en 5 segundos
+echo=                          16.abre el CMD {simblo del sistema}
+echo=                          17.abre el administrador de dispositivos
+echo=                          18.abre Firefox {si es que lo tienes instalado}
+echo=                          19.reinicia tu computadora
+echo=                          20.Ve a ver mas instruciones
+echo=                          21.abre el control de cuentas de usuario
+echo=                          {nivel de que tan estricto sera el UserAccountControlSettings}
+echo=                          22.Abrir el juego de Team Seas
+echo=                          23.Abrir windowframe (Juego)
+echo=                          24.Abrir Rolling DEMO (By Arddely Cespedes); (Juego)
+echo=                          25.Mas Juegos
+echo=                          26.Abrir Aplicaciones para tu PC
+echo=                          27.Saltate la verificacion del programa de instalacion
+echo=                          de windows 11.
+echo=                          28.Media Ramdom
+echo=                          29.Descargar Discord
+echo=                          31.Grabador De pantalla
+echo=                          30.salir
+echo            -------------------------------------------------------------------
+echo.     ------------------------------------------------------------------------------
+echo=                     presiona un numero y dale en la tecla Enter
+echo=                               Creditos:MrRecoveryy.
+echo.
+echo=             Copyright (C) 2021-2022. Mrblack Youtube; All rights reserved
+echo.
+echo.
+set /p menu=Numero u Opcion= 
+
+if "%menu%"=="1" goto op1
+if "%menu%"=="2" goto op2
+if "%menu%"=="3" goto op3
+if "%menu%"=="4" goto op4
+if "%menu%"=="5" goto op5
+if "%menu%"=="6" goto op6
+if "%menu%"=="7" goto op7
+if "%menu%"=="8" goto op8
+if "%menu%"=="9" goto op9
+if "%menu%"=="10" goto op10 
+if "%menu%"=="11" goto op11
+if "%menu%"=="12" goto op12
+if "%menu%"=="13" goto op13
+if "%menu%"=="14" goto op14
+if "%menu%"=="15" goto op15
+if "%menu%"=="16" goto op16
+if "%menu%"=="17" goto op17
+if "%menu%"=="18" goto op18
+if "%menu%"=="19" goto op19
+if "%menu%"=="20" goto op20
+if "%menu%"=="21" goto op21
+if "%menu%"=="22" goto op22
+if "%menu%"=="23" goto op23
+if "%menu%"=="24" goto op24
+if "%menu%"=="25" goto op25
+if "%menu%"=="26" goto op26
+if "%menu%"=="27" goto op27
+if "%menu%"=="28" goto op28
+if "%menu%"=="29" goto op29
+if "%menu%"=="30" goto op30
+if "%menu%"=="31" goto salir
+:op1
+cls
+start calc.exe
+pause>nul
+cls
+goto inicio
+
+:op3
+cls
+start control
+pause>nul
+cls
+goto inicio
+echo enter
+
+:op2
+cls
+echo.
+set /p buscar=Buscar=
+start https://www.youtube.com/results?search_query=%buscar%
+pause>nul
+cls
+goto inicio
+
+:salir
+cls&exit
+
+
+:op4
+cls
+start taskmgr
+pause>nul
+cls
+goto inicio
+
+
+:op5
+cls
+start explorer
+pause>nul
+cls
+goto inicio
+
+
+
+:op6
+cls
+start notepad
+pause>nul
+cls
+goto inicio
+
+
+
+:op7
+cls
+start winver
+pause>nul
+cls
+goto inicio
+
+
+
+:op8
+cls
+start mspaint
+pause>nul
+cls
+goto inicio
+
+
+
+:op9
+cls
+echo.
+start chrome
+pause>nul
+cls
+goto inicio
+
+
+
+:op10
+cls
+start winword
+pause>nul
+cls
+goto inicio
+
+
+:op11
+cls
+echo.
+start dxdiag
+pause>nul
+cls
+goto inicio
+
+
+
+
+:op12
+cls
+echo
+start explorer.exe "%appdata%"
+pause>nul
+cls
+goto inicio
+
+
+
+:op13
+cls
+start Excel
+pause>nul
+cls
+goto inicio
+
+
+
+
+
+:op14
+cls
+start MSCONFIG
+pause>nul
+cls
+goto inicio
+
+
+
+
+:op15
+cls
+echo.
+start shutdown.exe -s -t 10 -c "Tu pc se apagara en 10 segundos"
+pause>nul
+cls
+goto inicio
+
+
+
+
+:op16
+cls
+start cmd
+pause>nul
+cls
+goto inicio
+
+
+:op17
+cls
+start  devmgmt. msc
+pause>nul
+cls
+goto inicio
+
+
+
+
+:op18
+cls
+start Firefox
+pause>nul
+cls
+goto inicio
+
+
+
+
+:op19
+cls
+start Shutdown.exe -r
+pause>nul
+cls
+goto inicio
+
+:op20
+cls
+start data/Instrucciones.txt
+pause>nul
+cls
+goto inicio
+
+:op21
+cls
+start UserAccountControlSettings
+pause>nul
+cls
+goto inicio
+
+:op22
+cls
+start data/Games/TeamSeas/TeamSeas.exe
+pause>nul
+cls
+goto inicio
+
+:op23
+cls
+start data/Games/"windowframe accessible.exe"
+pause>nul
+cls
+goto inicio
+
+:op24
+cls
+start data/Games/Juego/"Rolling1.exe"
+pause>nul
+cls
+goto inicio
+
+:op25
+cls
+start explorer.exe “\data\Games\MasJuegos"
+pause>nul
+cls
+goto inicio
+
+:op26
+cls
+start explorer.exe “\data\executables"
+pause>nul
+cls
+goto inicio
+
+:op27
+cls
+start data\Scrips\"Skip_TPM_Check_on_Dynamic_Update_v2.cmd"
+pause>nul
+cls
+goto inicio
+
+:op28
+cls
+start explorer.exe “\data\Media"
+pause>nul
+cls
+goto inicio
+
+:op29
+cls
+start explorer.exe data\executables\DiscordSetup.exe
+pause>nul
+cls
+goto inicio
+
+:op30
+cls
+start \data\GrabadorPantalla\bin\Debug\GrabadorPantalla.exe
+pause>nul
+cls
+goto inicio
